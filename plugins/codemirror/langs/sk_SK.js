42a61522dd3e8289377664e9917c0f3207c7b9bd
ppatrik <patrik.pekarcik@upjs.sk>
2017-08-29 18:37:03 +0200

    repository cleanup and added slovak language

---
+++ plugins/codemirror/langs/sk_SK.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('sk_SK',{
+	'HTML source code': 'Zdrojový kód',
+	'Start search': 'Hľadať',
+	'Find next': 'Hľadať ďalej',
+	'Find previous': 'Hľadať nad',
+	'Replace': 'Nahradiť',
+	'Replace all': 'Nahradiť všetko'
+});
